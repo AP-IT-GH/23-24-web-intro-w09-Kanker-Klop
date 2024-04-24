70848c1a3cd2570ae412d3dc9ed5a00ce0eaca09
Kanker-Klop <150671397+Kanker-Klop@users.noreply.github.com>
2024-04-24 10:03:42 +0200

    Joa

--- oefening-01-02/js/mapScript.js
+++ oefening-01-02/js/mapScript.js
@@ -1,16 +1,20 @@
-/* In dit script plaats je de code om de kaart in de id apMap te tekenen, zodat de AP-Hogeschool met adres Ellermanstraat 33 gecentreerd staat.  
-* Gebruik hiervoor de documentatie op https://leafletjs.com/ 
-* Verander in de code online alle "var" in "let".
-*/
+let map = L.map('apMap').setView([51.23009, 4.41616], 17);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
+L.marker([51.23009, 4.41616]).addTo(map)
+    .bindPopup('AP-Hogeschool<br> Ellermanstraat 33')
+    .openPopup();
 
+var bounds = [[51.23041, 4.4155], [51.22991, 4.41675]];
 
-
-
-
-
-
+// create an orange rectangle
+L.rectangle(bounds, {color: "#ff0000", weight: 1}).addTo(map);
+    
+// zoom the map to the rectangle bounds
+map.fitBounds(bounds);
 
 
 // bepaal de rechthoek rondom het gebouw van AP
